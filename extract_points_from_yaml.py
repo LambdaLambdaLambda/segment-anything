d69087d9ea1780e3a367f4b54b2c97efb4aaed2d
Alberto Carraro <alberto.carraro@itiszuccante.edu.it>
2023-06-14 09:50:08 +0200

    sto per lanciare sam predictor

--- extract_points_from_yaml.py
+++ extract_points_from_yaml.py
@@ -2,6 +2,12 @@ import yaml
 import io
 import os
 import numpy as np
+from enum import Enum
+
+class Labels(Enum):
+    BACKGROUND = 0
+    WEED = 1
+    CROP = 2
 
 CWFID_dataset = {
     'annotations': os.path.join(*['CWFID_dataset', 'annotations']),
@@ -21,8 +27,18 @@ for filename in yaml_file_list:
         data_loaded = yaml.safe_load(stream)
         print(f"{data_loaded['filename']}")
         for rec in data_loaded['annotation']:
-            coordinates = list(zip(rec['points']['x'], rec['points']['y']))
+            print(f"{rec['type']}")
+            try: # some annotations do not contain coordinates represented as lists
+                iterator = iter(rec['points']['x'])
+                iterator = iter(rec['points']['y'])
+            except TypeError:
+                coordinates = [(rec['points']['x'], rec['points']['y'])]
+            else:
+                coordinates = list(zip(rec['points']['x'], rec['points']['y']))
             n = len(coordinates)
-            labels = np.empty(n)
-            labels.fill(1)
+            labels = np.empty(n) #prepare the array of labels as requesed by SAM in predictor mode
+            if rec['type'] == 'weed':
+                labels.fill(Labels.WEED.value)
+            elif rec['type'] == 'crop':
+                labels.fill(Labels.CROP.value)
         pass
