19bf4f76518693be61fd6731029d42f3484314ae
Alberto Carraro <alberto.carraro@itiszuccante.edu.it>
2023-06-14 10:01:14 +0200

    sto per usare predictor

--- extract_points_from_yaml.py
+++ extract_points_from_yaml.py
@@ -1,8 +1,12 @@
 import yaml
 import io
 import os
-import numpy as np
 from enum import Enum
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
 class Labels(Enum):
     BACKGROUND = 0
@@ -16,16 +20,18 @@ CWFID_dataset = {
     'SAM_masks': os.path.join(*['CWFID_dataset', 'SAM_masks'])
 }
 
-
-yaml_folder = CWFID_dataset['annotations']
-yaml_file_list = os.listdir(yaml_folder)
-yaml_file_list = [os.path.join(*[yaml_folder, f]) for f in yaml_file_list if f.endswith('.yaml') and not f.startswith('._')]
+yaml_file_list = os.listdir(CWFID_dataset['annotations'])
+yaml_file_list = [os.path.join(*[CWFID_dataset['annotations'], f]) for f in yaml_file_list if
+                  f.endswith('.yaml')
+                  and not f.startswith('._')
+                  and os.path.isfile(os.path.join(*[CWFID_dataset['annotations'], f]))
+                  ]
 yaml_file_list.sort() # contains ordered list of full paths only of yaml files inside yaml_folder
 
 for filename in yaml_file_list:
     with open(filename, 'r') as stream:
         data_loaded = yaml.safe_load(stream)
-        print(f"{data_loaded['filename']}")
+        print(f"Full name: {os.path.join(*[CWFID_dataset['annotations'], data_loaded['filename']])}")
         for rec in data_loaded['annotation']:
             print(f"{rec['type']}")
             try: # some annotations do not contain coordinates represented as lists
