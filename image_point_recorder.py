169e596235df7415af25f19bcf6ee3d1400e410a
Alberto Carraro <alberto.carraro@itiszuccante.edu.it>
2023-06-15 12:27:10 +0200

    Update image_point_recorder.py

--- image_point_recorder.py
+++ image_point_recorder.py
@@ -1,6 +1,15 @@
 # importing the module
 import cv2
 import os
+import json
+
+CWFID_dataset = {
+        'annotations': os.path.join(*['CWFID_dataset', 'annotations']),
+        'SAM_annotations': os.path.join(*['CWFID_dataset', 'SAM_annotations']),
+        'images': os.path.join(*['CWFID_dataset', 'images']),
+        'masks': os.path.join(*['CWFID_dataset', 'masks']),
+        'SAM_masks': os.path.join(*['CWFID_dataset', 'SAM_masks'])
+    }
 
 # function to display the coordinates of
 # of the points clicked on the image
@@ -10,6 +19,15 @@ def click_event(event, x, y, flags, params):
         # displaying the coordinates
         # on the Shell
         print(x, ' ', y)
+        # Data to be written
+        dictionary = {
+            'filename': os.path.join(*['CWFID_dataset', 'SAM_annotations', '001_annotation.json']),
+            'add_points': [],
+            'rem_ponts': []
+        }
+
+        with open("sample.json", "w") as outfile:
+            json.dump(dictionary, outfile)
 
         # displaying the coordinates
         # on the image window
@@ -40,13 +58,6 @@ def click_event(event, x, y, flags, params):
 
 # driver function
 if __name__ == "__main__":
-    CWFID_dataset = {
-        'annotations': os.path.join(*['CWFID_dataset', 'annotations']),
-        'SAM_annotations': os.path.join(*['CWFID_dataset', 'SAM_annotations']),
-        'images': os.path.join(*['CWFID_dataset', 'images']),
-        'masks': os.path.join(*['CWFID_dataset', 'masks']),
-        'SAM_masks': os.path.join(*['CWFID_dataset', 'SAM_masks'])
-    }
     img_file_list = os.listdir(CWFID_dataset['images'])
     img_file_list = [os.path.join(*[CWFID_dataset['images'], f]) for f in img_file_list if
                       f.endswith('.png')
